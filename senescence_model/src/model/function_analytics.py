2c8aca0edce42f7dacc187a3a91e3444426c5a04
CiaranWelsh <c.welsh2@newcastle.ac.uk>
2018-10-05 18:31:57 +0100

    removed xml output files from git

--- senescence_model/src/model/function_analytics.py
+++ senescence_model/src/model/function_analytics.py
@@ -2,17 +2,20 @@ import pandas, numpy
 import os, glob
 import xml.etree.ElementTree as ET
 from shutil import move
+import matplotlib.pyplot as plt
+import seaborn
 
 
 def get_files(directory):
-    return glob.glob('[0-9]*.xml')
+    return glob.glob(os.path.join(directory, '[0-9]*.xml'))
 
 
 def move_files_to(files, new_dir):
     os.makedirs(new_dir) if not os.path.isdir(new_dir) else None
+    if files == []:
+        raise ValueError
     for i in files:
-        if os.path.isfile(i):
-            move(i, new_dir)
+        move(i, new_dir)
 
 
 def parse_element(element, parsed=None):
@@ -45,6 +48,13 @@ def read_one_xml(file):
 
     tdf = tdf.drop(['xagent', 'name'], axis=1)
     fdf = fdf.drop(['xagent', 'name'], axis=1)
+
+    for i in tdf:
+        tdf[i] = pandas.to_numeric(tdf[i])
+
+    for i in fdf:
+        fdf[i] = pandas.to_numeric(fdf[i])
+
     tdf = tdf.set_index(['id', 'x', 'y', 'z'])
     fdf = fdf.set_index(['id'])
     return tdf, fdf
@@ -65,17 +75,54 @@ def read_xml_data(files):
     return tdf, fdf
 
 
+def plot_tissue_damage(tdf, id):
+    if isinstance(id, int):
+        id = [id]
+
+    fig = plt.figure()
+    for i in id:
+        plt.plot(list(tdf.columns), tdf.loc[i].values[0])
+        plt.xlabel('Simulation time')
+        plt.ylabel('Amount of Damage')
+
+    return fig
+
+
 if __name__ == '__main__':
     model_dir = r'D:\Documents\SenescenceModel\senescence_model\src\model'
 
     xml_output_dir = r'D:\Documents\SenescenceModel\senescence_model\src\model\results\xml_output'
 
-    files = get_files(model_dir)
-    if files == []:
+    try:
+        files = get_files(model_dir)
+        move_files_to(files, xml_output_dir)
+    except ValueError:
         files = get_files(xml_output_dir)
 
     tdf, fdf = read_xml_data(files)
 
+    # print(tdf.loc[0])
 
-    print(fdf)
+
+    fig = plot_tissue_damage(tdf, range(100))
+
+    plt.show()
+
+
+
+
+
+    tdf_fname = os.path.join(os.path.dirname(xml_output_dir), 'tissue_data.csv')
+    fdf_fname = os.path.join(os.path.dirname(xml_output_dir), 'fibroblast_data.csv')
+
+    tdf.to_csv(tdf_fname)
+    fdf.to_csv(fdf_fname)
+
+
+
+
+
+
+
+
 
